4802b69fe6a731d56720f50d21b7a20610ba7e75
Aymen Dirar <adirar111@gmail.com>
2020-05-30 18:38:55 -0700

    added constructor in spotify_client.py

--- spotify_client.py
+++ spotify_client.py
@@ -1,5 +1,20 @@
+import os
+import sys
+import json
+import webbrowser
 import spotipy
+from spotipy import util
+from secrets import spotify_client_id, spotify_client_secret, spotify_username
 
 class SpotifyClient:
     def __init__(self):
-        pass
+        scope = "playlist-modify-private"
+        redirect_uri = "http://google.com/"
+        self.token = util.prompt_for_user_token(spotify_username, scope, spotify_client_id, spotify_client_secret, redirect_uri)
+        spotifyObject = spotipy.Spotify(auth=self.token)
+        
+try:
+    client = SpotifyClient()
+    print("worked")
+except:
+    print("error occurred")
