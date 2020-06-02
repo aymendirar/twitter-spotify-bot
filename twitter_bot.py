264274a1e80b537de5d4d4361fa16d65ad4dd5ec
Aymen Dirar <adirar111@gmail.com>
2020-06-01 19:36:09 -0700

    edited twitter_bot.py

--- twitter_bot.py
+++ twitter_bot.py
@@ -3,7 +3,6 @@ import time
 
 from secrets import twitter_api_key, twitter_api_secret_key, twitter_access_token, twitter_access_token_secret
 
-<<<<<<< HEAD
     # EFFECTS: sets up client for use
 def initialize_bot(self):
     auth = tweepy.OAuthHandler(twitter_api_key, twitter_api_secret_key)
@@ -40,44 +39,3 @@ def get_mentions(self):
         type(tweet_content)
         print(tweet_content.split(" "))
         
-=======
-class TwitterClient:
-    # EFFECTS: sets up client for use
-    def __init__(self):
-        auth = tweepy.OAuthHandler(twitter_api_key, twitter_api_secret_key)
-        auth.set_access_token(twitter_access_token, twitter_access_token_secret)
-        self.twitter_api = tweepy.API(auth)
-        self.FILE_NAME = 'last_seen_id.txt'
-        self.last_seen_id = None    
-        
-    # EFFECTS: gets mentions 
-    def get_mentions(self):
-        mentions = self.twitter_api.mentions_timeline(self.last_seen_id, tweet_mode ="extentded")
-        
-        for mention in reversed(mentions):
-            tweet_content = mention.text
-            print(tweet_content)
-            type(tweet_content)
-            print(tweet_content.split(" "))
-            
-    # EFFECTS: gets the last seen id that was tweeted at the bot
-    def get_last_seen_id(self):
-        f_read = open(self.FILE_NAME, "r")
-        last_seen_id = int(f_read.read().strip())
-        f_read.close()
-        return last_seen_id
-    
-    # EFFECTS: writes the new last seen id to the last_seen_id file
-    def store_last_seen_id(self):
-        f_write = open(self.FILE_NAME, "w")
-        f_write.write(str(self.last_seen_id))
-        f_write.close()
-        return
-    
-    def reply_back(self, playlist_link):
-        pass
-        # send out tweet with @ saying here's your link
-        
-bot = TwitterClient()
-bot.get_mentions()
->>>>>>> 74e61c78bcbb94d0e8b04d4ee9cc49e00d151561
